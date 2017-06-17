a021db07d17f5c36a0a89eb885a4867595a1f3f6
Iman <imanhamidi91@gmail.com>
2017-06-17 08:53:15 -0700

    fix classname

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,32 +1,31 @@
 import React from "react";
 // Import sub-components
-
-// Helper Function
+import Search from "./Search";
 
 class Main extends React.Component {
   
   render() {
     return (
-    	<div class="container">
-    		<nav class="navbar navbar-inverse">
-	  			<div class="container-fluid">
-	    			<div class="navbar-header">
-				      <a class="navbar-brand" href="#">Quotes App</a>
+    	<div className="container">
+    		<nav className="navbar navbar-inverse">
+	  			<div className="container-fluid">
+	    			<div className="navbar-header">
+				      <a className="navbar-brand" href="#">Quotes App</a>
 				    </div>
-				    <ul class="nav navbar-nav">
+				    <ul className="nav navbar-nav">
 				      <li><a href="#">Home</a></li>
 				      <li><a href="#">Favorites</a></li>
 				    </ul>
 	   			</div>
 	    	</nav>
 
-		    <div class="container">
-		    	<div class="col col-md-4 col-md-offset-4">
+		    <div className="container">
+		    	<div className="col col-md-4 col-md-offset-4">
 		    		<p style="font-size: 21px;"><strong>Add a quote below</strong></p>
 					<form>
 		    			<input type="text" name="quote" size="45" placeholder="Type quote here"/>
 		    		</form>
-		    		<button class="btn btn-primary btn-md" style="margin-top:20px;float:right;">Submit</button>
+		    		<button className="btn btn-primary btn-md" style="margin-top:20px;float:right;">Submit</button>
 		    	</div>
 	    	</div>
 	    	</div>
